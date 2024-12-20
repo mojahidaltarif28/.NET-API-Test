503fe55840f210732acd095d8c97b0d52b57e77b
MD. Mojahid AL Tarif <112344739+mojahidaltarif28@users.noreply.github.com>
2024-12-20 20:55:02 +0600

    8:54

--- WEBBLAZOR/obj/Debug/net8.0/WEBBLAZOR.AssemblyInfo.cs
+++ WEBBLAZOR/obj/Debug/net8.0/WEBBLAZOR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBLAZOR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46313e9c97a91e09b3e5c1284ffc4b3de7b00191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ea17a306f351ed5fdfb565ba7575a5029e9851")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBLAZOR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBLAZOR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
