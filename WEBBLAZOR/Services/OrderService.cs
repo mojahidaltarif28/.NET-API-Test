58ea17a306f351ed5fdfb565ba7575a5029e9851
MD. Mojahid AL Tarif <112344739+mojahidaltarif28@users.noreply.github.com>
2024-12-20 19:14:10 +0600

    7:14

--- WEBBLAZOR/Services/OrderService.cs
+++ WEBBLAZOR/Services/OrderService.cs
@@ -6,34 +6,62 @@ using Newtonsoft.Json;
 
 namespace WEBBLAZOR.Services
 {
-    public class OrderService
+   public class OrderService
     {
         private readonly HttpClient _httpClient;
         public OrderService(HttpClient httpClient)
         {
-            _httpClient=httpClient;
+            _httpClient = httpClient;
         }
         public async Task<List<Product>> GetProductsAsync()
         {
-            try{
-                var response=await _httpClient.GetAsync("api/Order");
-                var jsonData=await response.Content.ReadAsStringAsync();
-                if(response.IsSuccessStatusCode)
+            try
+            {
+                var response = await _httpClient.GetAsync("api/Order");
+                var jsonData = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
                 {
-                    var products=JsonConvert.DeserializeObject<List<Product>>(jsonData);
+                    var products = JsonConvert.DeserializeObject<List<Product>>(jsonData);
                     return products;
                 }
-
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"Error in getproduct:{e.Message}");
             }
             return new List<Product>();
         }
+        public async Task<List<Ordered>> GetOrderedsAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("api/Ordered");
+                var jsonData = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    var ordered = JsonConvert.DeserializeObject<List<Ordered>>(jsonData);
+                    return ordered;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error in getOrdered:{e.Message}");
+            }
+            return new List<Ordered>();
+        }
     }
-    public class Product{
-          public int ProductId { get; set; }
+    public class Product
+    {
+        
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
+    }
+    public class Ordered
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int OrderQuantity { get; set; }
+        public DateTime OrderDate { get; set; }
     }
 }
--- WEBWASM/obj/Debug/net8.0/WEBWASM.AssemblyInfo.cs
+++ WEBWASM/obj/Debug/net8.0/WEBWASM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBWASM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d700aa23602d2344d0bac98deea8f72a3989e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46313e9c97a91e09b3e5c1284ffc4b3de7b00191")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBWASM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBWASM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
